e12898585916cc86c3f3126bacafb9bed4ed8be5
Alex Shepel <iam.alex.shepel@gmail.com>
2021-11-25 09:20:50 +0200

    fix deploy bug

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-import ghpages from 'gh-pages';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import './index.css';
-
-ghpages.publish('dist', function (err) {});
 
 ReactDOM.render(
   <React.StrictMode>
