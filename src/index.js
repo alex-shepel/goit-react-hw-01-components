20da21178e9802e590addc9ca5875f842b8532d1
Alex Shepel <iam.alex.shepel@gmail.com>
2021-11-25 08:59:53 +0200

    add gh-pages package

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+import ghpages from 'gh-pages';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import './index.css';
+
+ghpages.publish('dist', function (err) {});
 
 ReactDOM.render(
   <React.StrictMode>
